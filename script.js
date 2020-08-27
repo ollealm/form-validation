b17334661dfb3841fb6c8a11acfeb609f61594bd
Olle Alm <olle.alm@me.com>
2020-08-27 23:48:26 +0200

    adding focus and separating conditionals

--- script.js
+++ script.js
@@ -5,25 +5,50 @@ const validateForm = () => {
   const terms = document.forms["contact"]["terms"].checked;
   const message = document.forms["contact"]["message"].value;
   const regex = /\S+\@\S+\.\S+/; // kollar ???@???.???
-  console.log(message);
+
   if (name === "") {
     document.getElementById("message-name").innerHTML = "Name can't be empty";
+    document.getElementById("name-id").focus();
     return false;
-  } else if (email === "") {
+  } else {
+    document.getElementById("message-name").innerHTML = "";
+  }
+
+  if (email === "") {
     document.getElementById("message-email").innerHTML = "Email can't be empty";
+    document.getElementById("e-mail-id").focus();
     return false;
   } else if (!regex.test(email)) {
     document.getElementById("message-email").innerHTML =
       "Enter a correct email";
+    document.getElementById("e-mail-id").focus();
     return false;
-  } else if (type === "") {
+  } else {
+    document.getElementById("message-email").innerHTML = "";
+  }
+
+  if (type === "") {
     document.getElementById("message-type").innerHTML = "Select contact reason";
+    document.getElementById("private").focus();
     return false;
-  } else if (terms === false) {
+  } else {
+    document.getElementById("message-type").innerHTML = "";
+  }
+
+  if (terms === false) {
     document.getElementById("message-terms").innerHTML = "Accept terms";
+    document.getElementById("terms").focus();
     return false;
-  } else if (message === "") {
-    document.getElementById("message").innerHTML = "Please, leave a message";
+  } else {
+    document.getElementById("message-terms").innerHTML = "";
+  }
+
+  if (message.length < 5) {
+    document.getElementById("message").innerHTML =
+      "Please, leave a message. <br>(Minimum 5 charaters)";
+    document.getElementById("message-input").focus();
     return false;
+  } else {
+    document.getElementById("message").innerHTML = "";
   }
 };
